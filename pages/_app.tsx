9a035392ba3aa7a2526111459e0afaf4398273ca
Jasmine <jazicorndev@gmail.com>
2022-05-22 17:39:56 -0400

    added auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../comps/Layout'
+import { SessionProvider } from 'next-auth/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ 
+  Component, 
+  pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    < SessionProvider session = {pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   )
 }
 
-export default MyApp
+
