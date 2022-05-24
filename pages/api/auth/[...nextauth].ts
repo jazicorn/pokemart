2b8cd4e5635c45c209f7fd40e55b5ffd8d135f59
Jasmine <jazicorndev@gmail.com>
2022-05-24 17:00:35 -0400

    added customizeable options to nextauth file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,6 +5,14 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
+// nextauth-prisma adapter
+// https://next-auth.js.org/adapters/prisma
+
+// add auth pages
+// https://next-auth.js.org/configuration/pages
+
+// nextjs-nextauth example
+// https://github.com/nextauthjs/next-auth-example
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -12,7 +20,31 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    
+    }),  
   ],
+  pages: {
+    // signIn: '/auth/signin',  // Displays signin buttons
+    // signOut: '/auth/signout', // Displays form with sign out button
+    // error: '/auth/error', // Error code passed in query string as ?error=
+    // verifyRequest: '/auth/verify-request', // Used for check email page
+    // newUser: null // If set, new users will be directed here on first sign in
+  },
+
+  // Callbacks are asynchronous functions you can use to control what happens
+  // when an action is performed.
+  // https://next-auth.js.org/configuration/callbacks
+  callbacks: {
+    // async signIn(user, account, profile) { return true },
+    // async redirect(url, baseUrl) { return baseUrl },
+    // async session(session, user) { return session },
+    // async jwt(token, user, account, profile, isNewUser) { return token }
+  },
+
+  // You can set the theme to 'light', 'dark' or use 'auto' to default to the
+  // whatever prefers-color-scheme is set to in the browser. Default is 'auto'
+  theme: {
+    colorScheme: 'auto',
+  } 
+
 })
+
