4d1827a687f19213eb914f6e170e3ae1a72c3bc4
Jasmine <jazicorndev@gmail.com>
2022-05-24 20:46:13 -0400

    added email provider to nextauth file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import EmailProvider from "next-auth/providers/email"
+
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
@@ -14,14 +16,60 @@ const prisma = new PrismaClient()
 // nextjs-nextauth example
 // https://github.com/nextauthjs/next-auth-example
 
+// nextjs-middleware for security
+// https://docs-git-misc-docs-nextauthjs.vercel.app/configuration/nextjs#middleware
+
+// example of implementing session & jwt
+// https://hasura.io/blog/next-js-jwt-authentication-with-next-auth-and-integration-with-hasura/
+// another one
+// https://dev.to/mabaranowski/nextjs-authentication-jwt-refresh-token-rotation-with-nextauthjs-5696
+// another
+// https://strapi.io/blog/user-authentication-in-next-js-with-strapi
+
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),  
   ],
+  session: {
+    // Use JSON Web Tokens for session instead of database sessions.
+    // This option can be used with or without a database for users/accounts.
+    // Note: `strategy` should be set to 'jwt' if no database is used.
+    strategy: 'jwt'
+
+    // Seconds - How long until an idle session expires and is no longer valid.
+    // maxAge: 30 * 24 * 60 * 60, // 30 days
+
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    // updateAge: 24 * 60 * 60, // 24 hours
+  },
+
+  // JSON Web tokens are only used for sessions if the `strategy: 'jwt'` session
+  // option is set - or by default if no database is specified.
+  // https://next-auth.js.org/configuration/options#jwt
+  jwt: {
+    // A secret to use for key generation (you should set this explicitly)
+    secret: process.env.NEXTAUTH_SECRET,
+    // Set to true to use encryption (default: false)
+    // encryption: true,
+    // You can define your own encode/decode functions for signing and encryption
+    // if you want to override the default behaviour.
+    // encode: async ({ secret, token, maxAge }) => {
+    //  
+    // },
+    // decode: async ({ secret, token, maxAge }) => {
+    //  
+    // },
+  },
   pages: {
     // signIn: '/auth/signin',  // Displays signin buttons
     // signOut: '/auth/signout', // Displays form with sign out button
