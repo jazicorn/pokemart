b27bd496ef4bba324e4a22e751868ef184f73571
Jasmine <jazicorndev@gmail.com>
2022-05-25 17:04:29 -0400

    configured next.config to take different parameters for production and development

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-module.exports = nextConfig
+// @ts-check
+
+/*@type {import('next').NextConfig}*/ 
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      /* development only config options here */
+      reactStrictMode: true,
+    }
+  }
+
+  return {
+    /* config options for all phases except development here */
+  }
+}
