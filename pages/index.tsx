f07114f000fbbcd9e700d430c8d34c54788ea6be
Jasmine <jazicorndev@gmail.com>
2022-05-29 20:16:21 -0400

    removed unused modules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -13,6 +12,7 @@ const Home: NextPage = () => {
       
       <main /*className={styles.main}*/>
         <h1>Hello World</h1>
+        
       </main>
 
     </div>
