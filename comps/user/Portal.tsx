28ae21a4fe4e97d27cf357dc0272e90373bd8ae0
Jasmine <jazicorndev@gmail.com>
2022-06-02 19:40:57 -0400

    removed chakraUI button from portal file

--- comps/user/Portal.tsx
+++ comps/user/Portal.tsx
@@ -1,18 +1,17 @@
 import { useSession, signIn, signOut } from "next-auth/react"
-import { Button } from '@chakra-ui/react'
 
 export default function Component() {
   const { data: session } = useSession()
   if (session) {
     return (
       <div className="Signout">
-        <Button onClick={() => signOut()}> Logout </Button>
+        <button onClick={() => signOut()}> Logout </button>
       </div>
     )
   }
   return (
     <div className="Signin">
-      <Button onClick={() => signIn()}> Login </Button>
+      <button onClick={() => signIn()}> Login </button>
     </div>
   )
 }
