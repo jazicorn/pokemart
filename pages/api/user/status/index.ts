2ffac43b39f2a1728057be8a26e4dc3439208a52
Jasmine <jazicorndev@gmail.com>
2022-06-04 23:34:34 -0400

    added req,res to status endpoints

--- pages/api/user/status/index.ts
+++ pages/api/user/status/index.ts
@@ -1,3 +1,16 @@
 // TODO
 // 1. Endpoint : user status
 // 2. Endpoint : user status (all sessions)
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const session = await getSession({req});
+    if (!session) {
+        res.status(401).send({ message: `Unauthorized` })
+    }
+    let body = req.body;
+    res.status(200).json({ message: 'Endpoint: status'})
+}
+
+
