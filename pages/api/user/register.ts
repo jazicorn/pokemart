f3a80d791c37d32ba79b4446f967d3c0e5f2823a
Jasmine <jazicorndev@gmail.com>
2022-06-04 20:56:30 -0400

    created register endpoint for user

--- pages/api/user/register.ts
+++ pages/api/user/register.ts
@@ -1,15 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
 import bcrypt from 'bcryptjs'
 
-const handler = async (req:any, res: any) => {
+export default async function handler (req:NextApiRequest, res: NextApiResponse) {
+    if(req.method != 'POST') {
+        throw new Error(
+            `The HTTP ${req.method} method is not supported at this route.`
+        );
+    }
     const body = req.body
-    const email = await prisma.user.findUnique({
+    const getUser = await prisma.user.findUnique({
         where: {
-          email: body.email
+          email: body.email,
         },
+        select: {
+            email: true,
+            name:true
+        }
     });
 
-    if (email) {
-        res.status(200).json({ message: 'Already registered'})
+    if(getUser.name) {
+        res.status(200).json({ message: 'Username already registered'})
+        return
+    }
+
+    if (getUser.email) {
+        res.status(200).json({ message: 'Email already registered'})
         return;
     }
 
@@ -27,7 +42,7 @@ const handler = async (req:any, res: any) => {
 
     // update user password
     await prisma.user.update({
-        where: email,
+        where: getUser,
         data: {
             password: password
         }
@@ -36,5 +51,3 @@ const handler = async (req:any, res: any) => {
 
 }
 
-
-export default handler
