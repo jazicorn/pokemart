11f2efd1577170f6923cc645d8948f51398e6995
Jasmine <jazicorndev@gmail.com>
2022-06-04 22:52:16 -0400

    created delete user endpoint

---
+++ pages/api/user/[id].ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/react';
+
+// DELETE api/user/[id]
+export default async function handler (req: NextApiRequest, res: NextApiResponse) {
+    const userId = req.query.id
+    const user = await prisma.user.findUnique({
+        where: {
+            id: userId
+        }
+    })
+
+    const session = await getSession({ req });
+
+    if (!session) {
+        res.status(401).send({ message: `Unauthorized` })
+    }
+
+    if(req.method != 'DELETE') {
+        res.status(405).send({ message: `The HTTP ${req.method} method is not supported at this route.` })
+
+    }
+    const userName = user.name;
+    await prisma.user.delete({
+        where: {
+          email: user.email,
+        },
+    })
+    res.status(200).send({ message: `${userName} is deleted`})
+    
+}
+
+
+
