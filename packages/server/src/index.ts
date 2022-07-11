885ced64afde3ec4ba41fa04b4f69b374dbf75b7
Jasmine Anderson <jazicorndev@gmail.com>
2022-07-10 23:55:34 -0400

    updated dev server port number

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.listen(port, () => {
   console.log(`application is running on port ${port}.`);
