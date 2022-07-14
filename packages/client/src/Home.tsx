d1c38f578bc156ea7ca6887eca423c6325d4dd66
Jasmine Anderson <jazicorndev@gmail.com>
2022-07-14 17:59:12 -0400

    removed default code

---
+++ packages/client/src/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './components/Nav';
+
+function Home() {
+  return (
+    <div className="Home">
+        <header className="Home-header"></header>
+        <Nav />
+    </div>
+  );
+}
+
+export default Home;
