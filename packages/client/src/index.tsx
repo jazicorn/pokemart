634554a4defdc052a2726b75f6e90eaec63728f6
Jasmine Anderson <jazicorndev@gmail.com>
2022-07-14 19:02:05 -0400

    created folder structure, pages and components

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../styles/index.css';
-import Home from './Home';
+import Home from './pages/site/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
