0489e430715156000ff3d98c34657aa605d10030
Jasmine Anderson <jazicorndev@gmail.com>
2022-09-17 01:02:39 -0400

    created page to request one user profile

--- pages/profiles/[user]/index.tsx
+++ pages/profiles/[user]/index.tsx
@@ -1,68 +1,35 @@
-import { useRouter } from "next/router";
 import type { NextPage } from 'next';
-// import { useState, useEffect } from 'react'
+import InferNextPropsType from "../../../lib/types/props"
 
-// might need this for par if use getServerSideProp:
-// {providers, csrfToken}:InferGetServerSidePropsType<typeof getServerSideProps>
+export const getServerSideProps = async (context) => {
+  const user = context.user
+  const url = process.env.NEXTAUTH_URL;
+  const res = await fetch(url + '/api/profile/' + user);
+
+  if (!res.ok) {
+    return {
+      props: {}
+    }
+  }
+  return { props: res }
+}
 
 // This page returns user profile
-const Profile: NextPage = () => {
-  // obtain user name from request
-  const router = useRouter()
-  const { user } = router.query
+const Profile: NextPage = ({data}:InferNextPropsType<typeof getServerSideProps>) => {
 
-  // pass user name to obtain user profile ID from getUser
-  // TODO can't use prisma on .tsx pages, have to call from api
-  // use fetch to call from an api endpoint or create one 22/10/16
-  // const res = await fetch(`/api/profile/${id}`);
-  // const profile = res.json;
-
-  // i think im going to change the code, dynamic to [id] instead of [name]
-  // then mask the route as the username instead of profile number
-  // god just gave me idea, create endpoint: api/profiles/[name]
-  async function getUser(user) {
-      const res = await fetch(`/api/profile/${user}`);
-      const getUser = res.json;
-      return getUser
-  }
-
-  // check user type
-  async function getType(setType) {
-    if(typeof setType === 'string'){
-      const setTypeName = getUser(setType);
-      return setTypeName;
+  function useData() {
+    if (data == null || undefined) {
+      return "No User"
     }
-    return null;
+    return data.name;
   }
-
-  // TODO: need to clean this mess up, I dont know what is doing what
-  async function getProfile(setName) {
-    // Append the ID as a parameter to the API endpoint.
-    const user = getUser(setName)
-    if (user == null) {
-      return null
-    }
-    const id = getType(user);
-    if(id == null) {
-      return null
-    }
-    const res = await fetch(`/api/profile/${id}`);
-    const profile = res.json;
-    // return user object as prop to profile page function
-    return profile
-  }
-
-  const setUser = getUser(user)
-  const setType = getType(setUser)
-  const setProfile = getProfile(setType)
-
-  if (setProfile == null) {
-    return <h1>No Profile</h1>
-  }
-
+  const getName = useData();
   return (
+      <div>
       <h1>Profile</h1>
-  )
-}
+      <h1>{getName}</h1>
+      </div>
+  );
+};
 
 export default Profile
