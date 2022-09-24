a6bd3071f71a41d70bfb168d0b5d91f57f63a8f7
Jasmine Anderson <jazicorndev@gmail.com>
2022-09-24 03:09:21 -0400

    removed unused code

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,9 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { useState } from 'react';
-import Link from 'next/link';
-
-// @ts-ignore
-export default function Register(res: NextApiResponse, req: NextApiRequest) {
+export default function Register() {
     // TODO: Register User
     // TODO: Request to /api/users/register endpoint
     // TODO: User once successfully registered, redirect user
