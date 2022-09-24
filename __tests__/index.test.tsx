eea4af7dc58489b33bda3516c5476d55e019f6d7
Jasmine Anderson <jazicorndev@gmail.com>
2022-09-23 20:12:13 -0400

    formatted test folder

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,10 +1,9 @@
-import { render, screen } from '@testing-library/react'
-import Home from '../pages/index'
-import '@testing-library/jest-dom'
+import { render } from '@testing-library/react';
+import Home from '../pages/index';
+import '@testing-library/jest-dom';
 
 describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
-
-  })
-})
+    it('renders a heading', () => {
+        render(<Home />);
+    });
+});
