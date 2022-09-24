0b541dedcdd22d724f992e65dde98b0a480d4d62
Jasmine Anderson <jazicorndev@gmail.com>
2022-09-24 17:19:11 -0400

    updated user profile res

--- pages/api/profiles/[user].ts
+++ pages/api/profiles/[user].ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../lib/prisma';
 import { Public } from '../../../lib/models/profiles/user';
 
 // GET api/profiles/[user]
@@ -25,7 +26,12 @@ export default async function handler(
             profile: true,
         },
     });
+    if (getProfile == null) {
+        res.status(404).send({
+            message: `Not Found.`,
+        });
+    }
 
     // if !session return user profile public fields only
-    res.status(200).send(getProfile);
+    res.status(200).json(getProfile);
 }
