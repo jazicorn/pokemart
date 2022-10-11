617744fc8e4e4c6ad4bd0d0c4fd8925c307c6a1e
jazicorn <40859840+jazicorn@users.noreply.github.com>
2022-10-11 16:36:12 -0400

    profiles page returns profiles

--- pages/profiles/index.tsx
+++ pages/profiles/index.tsx
@@ -1,5 +1,32 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetStaticProps } from 'next';
+import { InferGetStaticPropsType } from 'next';
+import { Profile } from '../../lib/interfaces/schema';
 
-const Profiles: NextPage = () => <h1>Profiles</h1>;
+// Get staticsideprops for profile data
+export const getStaticProps: GetStaticProps = async () => {
+    const res = await fetch(`${process.env.API_ROUTES_URL}/api/profiles/`);
+    const profiles = await res.json();
+    return {
+        props: {
+            profiles,
+        },
+    };
+};
+
+const Profiles: NextPage = ({
+    profiles,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+    return (
+        <div>
+            {Object.values(profiles).map((profile: Profile) => {
+                return (
+                    <div key={profile.id}>
+                        <>{profile.userName}</>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
 
 export default Profiles;
