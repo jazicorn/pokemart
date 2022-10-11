798ef5194baa57230fcdf8c6b38da92ec98997f6
jazicorn <40859840+jazicorn@users.noreply.github.com>
2022-10-11 14:37:37 -0400

    added register page

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,8 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 // import EmailProvider from "next-auth/providers/email"
 import CredentialsProvider from 'next-auth/providers/credentials';
+import GithubProvider from 'next-auth/providers/github';
+import TwitterProvider from 'next-auth/providers/twitter';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 import { compare } from 'bcryptjs';
@@ -112,17 +114,19 @@ export default NextAuth({
         //  clientId: process.env.FACEBOOK_ID,
         //  clientSecret: process.env.FACEBOOK_SECRET,
         // }),
-        // GithubProvider({
-        //  clientId: process.env.GITHUB_ID,
-        //  clientSecret: process.env.GITHUB_SECRET,
-        //  // https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps
-        //  // @ts-ignore
-        //  scope: "read:user",
-        // }),
-        // TwitterProvider({
-        //  clientId: process.env.TWITTER_ID,
-        //  clientSecret: process.env.TWITTER_SECRET,
-        // }),
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+            // https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            scope: 'read:user',
+        }),
+        TwitterProvider({
+            clientId: process.env.TWITTER_ID,
+            clientSecret: process.env.TWITTER_SECRET,
+            version: '2.0',
+        }),
     ],
     // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
     // https://next-auth.js.org/configuration/databases
--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,5 +1,130 @@
-export default function Register() {
+import type { NextApiRequest, NextApiResponse } from 'next';
+import {
+    getProviders,
+    signIn,
+    getSession,
+    getCsrfToken,
+} from 'next-auth/react';
+import { InferGetServerSidePropsType } from 'next';
+import { useState } from 'react';
+import Link from 'next/link';
+
+export async function getServerSideProps(context: {
+    req: NextApiRequest;
+    res: NextApiResponse;
+}) {
+    const { req } = context;
+    const session = await getSession({ req });
+
+    if (session) {
+        return {
+            redirect: { destination: '/' },
+        };
+    }
+    return {
+        props: {
+            csrfToken: await getCsrfToken(context),
+            providers: await getProviders(),
+        },
+    };
+}
+
+// TODO finish adding types to errors on this page
+// react types for events | https://stackoverflow.com/questions/42081549/typescript-react-event-types
+// Maybe add these types to my lib/types folder as exports?
+type InputEvent = React.ChangeEvent<HTMLInputElement>;
+type ButtonEvent = React.MouseEvent<HTMLButtonElement>;
+
+interface Profile {
+    id: string;
+    name: string;
+    clientId: string;
+    clientSecret: string;
+}
+
+export default function Register({
+    providers,
+    csrfToken,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
     // TODO: Register User
     // TODO: Request to /api/users/register endpoint
     // TODO: User once successfully registered, redirect user
+    // used flatmap to filter credentials from provider stack | https://www.samanthaming.com/tidbits/80-flatmap/
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState(null);
+    const signInUser = async (e) => {
+        e.preventDefault();
+        const options = { redirect: false, email, password };
+        const res = await signIn('credentials', options);
+        setMessage(null);
+
+        if (res?.error) {
+            setMessage(res?.error);
+        }
+        // return router.push('/')
+    };
+    return (
+        <div className="main">
+            <h1>Welcome</h1>
+
+            <div className="olduser">
+                <h3>Already Registered?</h3>
+
+                <Link href="/auth/signin">
+                    <a>Sign-In</a>
+                </Link>
+            </div>
+            <div className="credentials">
+                <form method="post" action="/api/auth/callback/credentials">
+                    <input
+                        name="csrfToken"
+                        type="hidden"
+                        defaultValue={csrfToken}
+                    />
+                    <h4>Email</h4>
+                    <label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={email}
+                            onChange={(e: InputEvent) =>
+                                setEmail(e.target.value)
+                            }
+                        />
+                    </label>
+
+                    <h4>Password</h4>
+                    <label>
+                        <input
+                            type="text"
+                            id="password"
+                            name="password"
+                            value={password}
+                            onChange={(e: InputEvent) =>
+                                setPassword(e.target.value)
+                            }
+                        />
+                    </label>
+                    <p style={{ color: 'red' }}>{message}</p>
+                    <button onClick={(e: ButtonEvent) => signInUser(e)}>
+                        Sign In
+                    </button>
+                </form>
+            </div>
+            {Object.values(providers).map((provider: Profile) => {
+                const ignore = 'Credentials' || 'Email';
+                return provider.name === ignore ? (
+                    []
+                ) : (
+                    <div key={provider.name}>
+                        <button onClick={() => signIn(provider.id)}>
+                            Register with {provider.name}
+                        </button>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
