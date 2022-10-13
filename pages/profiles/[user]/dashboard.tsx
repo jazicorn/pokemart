acb55cd61868d8073b6685b89cac40df38752551
jazicorn <40859840+jazicorn@users.noreply.github.com>
2022-10-12 21:52:27 -0400

    created dashboard page for user

--- pages/profiles/[user]/dashboard.tsx
+++ pages/profiles/[user]/dashboard.tsx
@@ -1,2 +1,44 @@
 // home for todo list
-export {};
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { InferGetServerSidePropsType } from 'next';
+import { getSession } from 'next-auth/react';
+
+// TODO: make sure correct data response received in session
+export async function getServerSideProps(context: {
+    req: NextApiRequest;
+    res: NextApiResponse;
+}) {
+    const { req } = context;
+    const session = await getSession({ req });
+
+    if (!session) {
+        return {
+            redirect: { destination: '/' },
+        };
+    }
+
+    const res = await fetch(`${process.env.API_ROUTES_URL}/api/user/`);
+    const user = await res.json();
+
+    return {
+        props: {
+            user,
+        },
+    };
+}
+
+// This page returns user profile
+const Dashboard = ({
+    user,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    if (user == null || undefined) {
+        return 'No User';
+    }
+    return (
+        <div>
+            <h1>Welcome!</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
