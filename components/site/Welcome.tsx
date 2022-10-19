8788e35621b8bed19c464d1eb51af9b3d22adea4
jazicorn <40859840+jazicorn@users.noreply.github.com>
2022-10-19 18:38:46 -0400

    changed formatting

--- components/site/Welcome.tsx
+++ components/site/Welcome.tsx
@@ -1,15 +1,15 @@
 export default function Welcome() {
     return (
         <div className="grow flex flex-col justify-between items-stretch bg-slate-100">
-            <div className="h-1/6 flex flex-row justify-center content-center bg-green-100">
+            <div className="basis-1/6 flex flex-row justify-center content-center bg-green-100">
                 <div className="flex flex-col justify-center content-center bg-pink-300 ">
                     <h1 className="justify-center content-center bg-purple-100">
                         Welcome to Template-NextJS-Full
                     </h1>
                 </div>
             </div>
-            <div className="grow flex flex-col justify-center content-center bg-pink-100">
-                <div className="p-40 bg-blue-100">
+            <div className=" basis-4/6 grow flex lg:flex-row sm:flex-col flex-wrap justify-center content-center bg-pink-100">
+                <div className="w-1/2 bg-purple-400">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                     do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
@@ -20,7 +20,7 @@ export default function Welcome() {
                     deserunt mollit anim id est laborum.
                 </div>
             </div>
-            <div className="h-1/6 bg-green-100">
+            <div className="basis-1/6 bg-green-100">
                 <div></div>
             </div>
         </div>
