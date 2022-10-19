0f6166a8d9a5bb8ad26502a34ad4387b89d9a23c
jazicorn <40859840+jazicorn@users.noreply.github.com>
2022-10-19 18:43:47 -0400

    extended default screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  content: [   
-    "./pages/**/*.{js,ts,jsx,tsx}",   
-    "./components/**/*.{js,ts,jsx,tsx}",  
+  content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
      ],
-  theme: {
-    extend: {},
-  },
+    theme: {
+        extend: {
+                screens: {
+                    'sm': '175px',
+                    'md': '475px',
+            },
+        },
+    },
   plugins: [],
 }
