31efbc020874737b7d2afeb2808efd521f1d0d55
jazicorn <40859840+jazicorn@users.noreply.github.com>
2022-10-29 17:14:28 -0400

    enabled top level await config to next.js webpack configuration

--- next.config.js
+++ next.config.js
@@ -40,6 +40,13 @@ module.exports = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+    },
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, ...{ topLevelAwait: true }};
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true
+    return config;
   },
 }
 
