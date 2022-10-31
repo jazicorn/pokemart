415890fc0144fbb2ba0d3914d92ff5b9c5b374d5
jazicorn <40859840+jazicorn@users.noreply.github.com>
2022-10-31 16:44:21 -0400

    added env constants to next.config file

--- next.config.js
+++ next.config.js
@@ -30,23 +30,26 @@ const nextConfig = {
 module.exports = withTM(nextConfig);
 
 module.exports = {
-  onDemandEntries: {
-    // period (in ms) where the server will keep pages in the buffer
-    maxInactiveAge: 25 * 1000,
-    // number of pages that should be kept simultaneously without being disposed
-    pagesBufferLength: 2,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    env: {
+        NEXT_PUBLIC_API_ROUTE: process.env.NEXT_PUBLIC_API_ROUTE
     },
-  webpack: (config) => {
-    // this will override the experiments
-    config.experiments = { ...config.experiments, ...{ topLevelAwait: true }};
-    // this will just update topLevelAwait property of config.experiments
-    // config.experiments.topLevelAwait = true
-    return config;
-  },
+    onDemandEntries: {
+        // period (in ms) where the server will keep pages in the buffer
+        maxInactiveAge: 25 * 1000,
+        // number of pages that should be kept simultaneously without being disposed
+        pagesBufferLength: 2,
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+        },
+    webpack: (config) => {
+        // this will override the experiments
+        config.experiments = { ...config.experiments, ...{ topLevelAwait: true }};
+        // this will just update topLevelAwait property of config.experiments
+        // config.experiments.topLevelAwait = true
+        return config;
+    },
 }
 
