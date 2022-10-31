7e5ca51f26764da2e83d0d4f53f9e2fec48361a2
jazicorn <40859840+jazicorn@users.noreply.github.com>
2022-10-30 21:52:30 -0400

    gen comp renders pokemon object info

--- components/site/Generator.tsx
+++ components/site/Generator.tsx
@@ -1,29 +1,51 @@
+// next/image wasnt working properly so I had to use <img/>
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+/**Tip: You CANNOT pass props (request data) directly from components, MUST pass props throgh NextPage */
+/**
+ *
+ * Example Pokemon Object
+ * {
+ * "id":137,
+ * "name":"porygon",
+ * "height":8,
+ * "weight":365,
+ * "base_experience":79,
+ * "is_default":true,
+ * "order":225,
+ * "pokemon_species_id":137,
+ * "stats":{"base_stat":65,"stat_id":1,"effort":0},
+ * "types":[
+ *      "0" : {
+ *          "pokemon_v2_type":{
+ *              "generation_id":1,
+ *              "name":"normal",
+ *              "id":1
+ *          }
+ *      }
+ * ],
+ * "sprites":{
+ *      "front_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/137.png",
+ *      "front_female":null,
+ *      "front_shiny":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/137.png",
+ *      "front_shiny_female":null,
+ *      "back_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/137.png",
+ *      "back_female":null,
+ *      "back_shiny":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/137.png",
+ *      "back_shiny_female":null,"other":null,"versions":null}
+ * }
+ */
+
 // Generate random pokemon from pokemon api
-import type { GetStaticProps } from 'next';
-import { InferGetStaticPropsType } from 'next';
-import { PokemonClient } from 'pokenode-ts';
-const pokedex = new PokemonClient();
-
-// Get staticsideprops for profile data
-export const pokemonData = await pokedex.getPokemonByName('luxray');
-
-export default function Welcome() {
-    console.log(pokemonData);
+const Generator = (pokeObj) => {
+    let sprite = pokeObj.pokemon.sprites.front_default;
     return (
-        <div className="grow flex flex-col justify-between items-stretch bg-slate-100">
-            <div className="basis-1/6 flex flex-row justify-center content-center bg-green-100">
-                <div className="flex flex-col justify-center content-center bg-pink-300 ">
-                    <h1 className="justify-center content-center bg-purple-100">
-                        Pokemon Generator
-                    </h1>
-                </div>
-            </div>
-            <div className=" basis-4/6 grow flex lg:flex-row sm:flex-col flex-wrap justify-center content-center bg-pink-100">
-                <div className="w-1/2 bg-purple-400">Pokemon:</div>
-            </div>
-            <div className="basis-1/6 bg-green-100">
-                <div></div>
-            </div>
+        <div className="basis-1/3 bg-indigo-300 h-full flex flex-col justify-start content-center ">
+            <h1 className="bg-teal-200 basis-5">Random Generator</h1>
+            <div className="bg-red-200">{pokeObj.pokemon.name}</div>
+            <img src={sprite}></img>
         </div>
     );
-}
+};
+
+export default Generator;
